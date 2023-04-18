dbd5206015c83b78f343ed152af7c3392bd1d553
vkazankov <vkazankov@inspiredtesting.com>
2023-04-18 23:19:25 +0300

    Updated user data

--- user_data.py
+++ user_data.py
@@ -1,8 +1,8 @@
 name_user = "VictorKazankov"
-token_user = "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"
+token_user = "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"
 headers = {"Authorization": f"Bearer {token_user}", "Accept": "application/vnd.github+json"}
 repository = "LearnQA_PythonAPI"
 
 
-another_user_token = "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"
+another_user_token = "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"
 headers_for_another_user = {"Authorization": f"Bearer {another_user_token}", "Accept": "application/vnd.github+json"}
